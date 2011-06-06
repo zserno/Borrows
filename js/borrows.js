3b88d406bca11fb34486995458a44467ba1d2934
Erno Zsemlye <zserno@gmail.com>
2011-06-06 03:42:38 +0200

    Add ajax callback to check availability.

--- js/borrows.js
+++ js/borrows.js
@@ -4,8 +4,16 @@
     $("#calendar .week input.form-checkbox", context).click(function() {
       $("#calendar", context).block({
         // @TODO make path dynamic.
-        message: '<img src="' + Drupal.settings.basePath + 'sites/all/modules/borrows/images/loader.gif" />',
+        message: '<img src="' + Drupal.settings.basePath + 'sites/all/modules/borrows/images/loader.gif" /><h2>' + Drupal.t("Checking availability...") + '</h2>',
+        css: {width: 'auto', padding: '5px'}
       });
+      var dataToSend = {'nid': '12'};
+      borrowsAjax(dataToSend);
     });
   }
 })(jQuery, Drupal)
+
+function borrowsAjax(dataToSend) {
+  $.getJSON(Drupal.settings.basePath + 'borrows_ajax/' + dataToSend.nid, function(data) {
+  });
+}
